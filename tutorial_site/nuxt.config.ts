b4d6714f2541b8b3c5f8af734fa02f0e5e58843e
ViliusP <viliuspaliokas@gmail.com>
2024-02-03 15:47:06 +0200

    Install typescript and vue-tsc

--- tutorial_site/nuxt.config.ts
+++ tutorial_site/nuxt.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath } from "url";
 import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
 
 export default defineNuxtConfig({
+  typescript: {
+    typeCheck: true
+  },
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.css",
@@ -17,7 +20,8 @@ export default defineNuxtConfig({
     async (options, nuxt) => {
       // @ts-ignore
       nuxt.hooks.hook("vite:extendConfig", (config) =>
-        config.plugins.push(vuetify())
+        // @ts-ignore
+        config.plugins?.push(vuetify())
       );
     },
   ],
