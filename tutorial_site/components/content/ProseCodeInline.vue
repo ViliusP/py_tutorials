2370f8aa92bb4c7a704e0c1b47e6c43eee574c40
ViliusP <viliuspaliokas@gmail.com>
2024-02-08 23:27:29 +0200

    Increase inline code font size

--- tutorial_site/components/content/ProseCodeInline.vue
+++ tutorial_site/components/content/ProseCodeInline.vue
@@ -1,3 +1,3 @@
 <template>
-    <code class="px-2 py-1 bg-grey-darken-4 rounded"><slot /></code>
+    <code style="font-size: 105%" class="px-2 py-1 bg-grey-darken-4 rounded"><slot /></code>
 </template>
