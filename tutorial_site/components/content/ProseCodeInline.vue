b59a6997376454eb7832c407ea22e4af27e66a32
ViliusP <viliuspaliokas@gmail.com>
2024-02-08 23:23:09 +0200

    Remove script from inline code component

--- tutorial_site/components/content/ProseCodeInline.vue
+++ tutorial_site/components/content/ProseCodeInline.vue
@@ -1,11 +1,3 @@
 <template>
     <code class="px-2 py-1 bg-grey-darken-4 rounded"><slot /></code>
 </template>
-
-<script setup>
-const props = defineProps();
-
-// Console log all props
-console.log(props);
-
-</script>
