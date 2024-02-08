5e14f21d2ce404cafc4a66342752acc13a76d252
ViliusP <viliuspaliokas@gmail.com>
2024-02-08 23:22:54 +0200

    Add inline code style

---
+++ tutorial_site/components/content/ProseCodeInline.vue
@@ -0,0 +1,11 @@
+<template>
+    <code class="px-2 py-1 bg-grey-darken-4 rounded"><slot /></code>
+</template>
+
+<script setup>
+const props = defineProps();
+
+// Console log all props
+console.log(props);
+
+</script>
