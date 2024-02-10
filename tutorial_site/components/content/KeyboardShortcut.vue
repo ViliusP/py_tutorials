f9143a4bc4054efb898783affec6fea7e6a154b0
ViliusP <viliuspaliokas@gmail.com>
2024-02-10 14:11:18 +0200

    Add initial keyboard shortcut component

---
+++ tutorial_site/components/content/KeyboardShortcut.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>
+    <!-- Only render if props.keys is defined and not empty -->
+    <template v-if="props.keys && props.keys.length > 0">
+      <template v-for="(key, index) in props.keys" :key="index">
+        <v-kbd>{{ key.toUpperCase() }}</v-kbd>
+        <!-- Conditionally render the plus character if it's not the last item -->
+        <span v-if="index < props.keys.length - 1"> + </span>
+      </template>
+    </template>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+// Declare the component's props, allowing keys to be string array, null, or undefined
+const props = defineProps<{
+  keys?: string[] | null;
+}>();
+</script>
