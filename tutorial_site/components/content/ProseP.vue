dedadba12596b10cf476231d409f0fc7ea100a4d
ViliusP <viliuspaliokas@gmail.com>
2024-02-10 20:29:50 +0200

    Add prose p

---
+++ tutorial_site/components/content/ProseP.vue
@@ -0,0 +1,16 @@
+<template>
+    <p class="prose-p text-justify">
+        <slot />
+    </p>
+</template>
+
+<style>
+.prose-p {
+    font-size: 1.375rem !important;
+    font-weight: 300;
+    line-height: 2rem;
+    letter-spacing: 0.002em !important;
+    font-family: "Noto Sans", "Roboto", sans-serif;
+    text-transform: none !important;
+}
+</style>
