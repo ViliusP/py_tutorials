60561793940c51c5058dd63fa3239ca2700c979d
ViliusP <viliuspaliokas@gmail.com>
2024-02-10 11:30:10 +0200

    Change TextHx to ProseHx

---
+++ tutorial_site/components/content/ProseH5.vue
@@ -0,0 +1,17 @@
+<template>
+    <h5 :id="id" class="text-h5 pb-4">
+        <a v-if="id && generate" :href="`#${id}`">
+            <slot />
+        </a>
+        <slot v-else />
+    </h5>
+</template>
+  
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports'
+
+const props = defineProps<{ id?: string }>()
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && headings?.anchorLinks?.h5)
+</script>
