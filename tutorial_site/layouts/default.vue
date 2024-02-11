93a410e43ffc1d05f19b5cbc7ba0c809e8918350
ViliusP <viliuspaliokas@gmail.com>
2024-02-11 13:13:46 +0200

    Make navigation drawer floating after certain breakpoint

--- tutorial_site/layouts/default.vue
+++ tutorial_site/layouts/default.vue
@@ -1,58 +1,61 @@
 <template>
-    <v-layout>
-        <v-app-bar>
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title>{{ $t('hello.world') }}</v-toolbar-title>
-        </v-app-bar>
+  <v-layout>
+      <v-app-bar>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title>{{ $t('hello.world') }}</v-toolbar-title>
+      </v-app-bar>
 
-        <v-navigation-drawer v-model="drawer" permanent>
+      <v-navigation-drawer v-model="drawer" :floating="mdAndUp">
 
 
-        <v-list
-          :lines="false"
-          density="compact"
-          nav
+      <v-list
+        :lines="false"
+        density="compact"
+        nav
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :value="item"
+          rounded="xl"
+          color="primary"
         >
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :value="item"
-            rounded="xl"
-            color="primary"
-          >
-            <template v-slot:prepend>
-              <v-icon :icon="item.icon"></v-icon>
-            </template>
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
 
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item>
-        </v-list>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item>
+      </v-list>
 
 
 
-        </v-navigation-drawer>
+      </v-navigation-drawer>
 
-        <v-main class="d-flex align-center justify-left" style="min-height: 300px;">
-            <slot />
-        </v-main>
-    </v-layout>
+      <v-main class="d-flex align-center justify-left" style="min-height: 300px;">
+          <slot />
+      </v-main>
+  </v-layout>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { mdAndUp } = useDisplay()
 
 const { locale } = useI18n()
 
 const drawer = ref(true)
 
 const items = [
-    { text: 'My Files', icon: 'mdi-folder' },
-    { text: 'Shared with me', icon: 'mdi-account-multiple' },
-    { text: 'Starred', icon: 'mdi-star' },
-    { text: 'Recent', icon: 'mdi-history' },
-    { text: 'Offline', icon: 'mdi-check-circle' },
-    { text: 'Uploads', icon: 'mdi-upload' },
-    { text: 'Backups', icon: 'mdi-cloud-upload' },
-  ]
+  { text: 'My Files', icon: 'mdi-folder' },
+  { text: 'Shared with me', icon: 'mdi-account-multiple' },
+  { text: 'Starred', icon: 'mdi-star' },
+  { text: 'Recent', icon: 'mdi-history' },
+  { text: 'Offline', icon: 'mdi-check-circle' },
+  { text: 'Uploads', icon: 'mdi-upload' },
+  { text: 'Backups', icon: 'mdi-cloud-upload' },
+]
 
 </script>
