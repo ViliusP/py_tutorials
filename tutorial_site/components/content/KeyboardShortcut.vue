dbec5808042aa9c9a8b1b36f38b71dffe47ad37d
ViliusP <viliuspaliokas@gmail.com>
2024-02-11 11:51:42 +0200

    Make keyboardshortcut and code inline monospaced

--- tutorial_site/components/content/KeyboardShortcut.vue
+++ tutorial_site/components/content/KeyboardShortcut.vue
@@ -1,11 +1,11 @@
 <template>
-  <span>
+  <span class="keyboard-shortcut">
     <!-- Only render if props.keys is defined and not empty -->
     <template v-if="props.keys && props.keys.length > 0">
       <template v-for="(key, index) in props.keys" :key="index">
         <v-kbd>{{ key.toUpperCase() }}</v-kbd>
         <!-- Conditionally render the plus character if it's not the last item -->
-        <span v-if="index < props.keys.length - 1"> + </span>
+        <span v-if="index < props.keys.length - 1">+</span>
       </template>
     </template>
   </span>
@@ -19,3 +19,9 @@ const props = defineProps<{
   keys?: string[] | null;
 }>();
 </script>
+
+<style>
+.keyboard-shortcut {
+  font-family: 'Noto Sans Mono', 'monospace';
+}
+</style>
--- tutorial_site/components/content/ProseCodeInline.vue
+++ tutorial_site/components/content/ProseCodeInline.vue
@@ -1,3 +1,10 @@
 <template>
-    <code class="px-1 bg-grey-darken-4 rounded"><slot /></code>
+    <code class="px-1 bg-grey-darken-4 rounded code-inline"><slot /></code>
 </template>
+
+<style>
+.code-inline *
+{
+  font-family: 'Noto Sans Mono', 'monospace';
+}
+</style>
