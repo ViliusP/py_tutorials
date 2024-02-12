b7d3edff2dd9e87860944e594f017382da2c2aa6
ViliusP <viliuspaliokas@gmail.com>
2024-02-12 12:41:49 +0200

    Fix v-kbd hydration

--- tutorial_site/components/content/KeyboardShortcut.vue
+++ tutorial_site/components/content/KeyboardShortcut.vue
@@ -1,10 +1,8 @@
 <template>
   <span class="keyboard-shortcut">
-    <!-- Only render if props.keys is defined and not empty -->
     <template v-if="props.keys && props.keys.length > 0">
       <template v-for="(key, index) in props.keys" :key="index">
-        <v-kbd>{{ key.toUpperCase() }}</v-kbd>
-        <!-- Conditionally render the plus character if it's not the last item -->
+        <kbd>{{ key }}</kbd>
         <span v-if="index < props.keys.length - 1">+</span>
       </template>
     </template>
@@ -14,7 +12,6 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 
-// Declare the component's props, allowing keys to be string array, null, or undefined
 const props = defineProps<{
   keys?: string[] | null;
 }>();
@@ -24,4 +21,17 @@ const props = defineProps<{
 .keyboard-shortcut {
   font-family: 'Noto Sans Mono', 'monospace';
 }
+
+.keyboard-shortcut kbd {
+  background: rgb(var(--v-theme-kbd));
+	color: rgb(var(--v-theme-on-kbd));
+	border-radius: 3px;
+	display: inline;
+	font-size: 85%;
+	font-weight: normal;
+	padding: 0.2em 0.4rem;
+	box-shadow: 0px 3px 1px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)), 0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)), 0px 1px 5px 0px var(--v-shadow-key-ambient-opacity, rgba(0, 0, 0, 0.12));
+  text-transform: uppercase !important;
+} 
+
 </style>
