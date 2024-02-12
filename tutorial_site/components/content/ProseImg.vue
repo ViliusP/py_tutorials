581b124bd66ae470e4d59a99b881d4318af2ccef
ViliusP <viliuspaliokas@gmail.com>
2024-02-12 14:15:15 +0200

    Add intitial ProseImg component

--- tutorial_site/components/content/ProseImg.vue
+++ tutorial_site/components/content/ProseImg.vue
@@ -1,59 +1,75 @@
 <template>
-    <v-sheet :border="true" :elevation="0" class="image-container" >
-        <img class="" :src="refinedSrc" :alt="alt" :width="width" :height="height">
-    </v-sheet>
-    <div v-if="props.alt" class="text-caption image-caption">
-        {{ props.alt}}
-    </div>
+  <!-- @vue-ignore -->
+  <v-img
+    :src="img(src, { intHeight, quality: 80 })"
+    :max-height="height"
+    :alt="alt"
+    :srcset="_srcset.srcset"
+    :sizes="_srcset.sizes"
+    position="left center"
+    cov
+  ></v-img>
+  <figcaption v-if="props.title" class="text-caption image-caption">
+    {{ props.title }}
+  </figcaption>
 </template>
-  
+
 <script setup lang="ts">
-import { withTrailingSlash, withLeadingSlash, joinURL } from 'ufo'
-import { useRuntimeConfig, computed } from '#imports'
+import { computed, useImage } from "#imports";
 
 const props = defineProps({
-    src: {
-        type: String,
-        default: ''
-    },
-    alt: {
-        type: String,
-        default: ''
-    },
-    width: {
-        type: [String, Number],
-        default: undefined
-    },
-    height: {
-        type: [String, Number],
-        default: undefined
-    }
-})
+  src: {
+    type: String,
+    default: "",
+  },
+  alt: {
+    type: String,
+    default: "",
+  },
+  title: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: [String, Number],
+    default: 600,
+  },
+  height: {
+    type: [String, Number],
+    default: 400,
+  },
+});
 
-const refinedSrc = computed(() => {
-    if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
-        const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
-        if (_base !== '/' && !props.src.startsWith(_base)) {
-            return joinURL(_base, props.src)
-        }
-    }
-    return props.src
-})
+const img = useImage();
+
+const _srcset = computed(() => {
+  return img.getSizes(props.src, {
+    sizes: "xs:100vw sm:90vw md:70vw lg:60vw xl:50vw",
+    modifiers: {
+      format: "webp",
+      quality: 70,
+      height: intVal(props.height),
+    },
+  });
+});
+
+const intHeight = computed(() => {
+  return intVal(props.height);
+});
+
+function intVal(n: number | string): number {
+  return typeof n === "number" ? n : parseInt(n, 10);
+}
 </script>
 
 <style>
-.image-container {
-  display: inline-flex; /* Or inline-block, depending on your layout needs */
-  padding: 0; /* Remove padding */
-  line-height: 0; /* For removing space below the image */
-}
 .image-caption {
-    margin-top: 4px;
-    font-size: 0.875rem !important;
-	font-weight: 400;
-	line-height: 1.25rem;
-	letter-spacing: 0.0333333333em !important;
-	font-family: "Noto Sans", "Roboto", sans-serif;
-	text-transform: none !important;
+  margin-top: 4px;
+  font-size: 0.875rem !important;
+  font-weight: 400;
+  line-height: 1.25rem;
+  letter-spacing: 0.0333333333em !important;
+  font-family: "Noto Sans", "Roboto", sans-serif;
+  text-transform: none !important;
 }
 </style>
