2344d0a0fc53b290de78ee6fb128a1359a7d0ced
ViliusP <viliuspaliokas@gmail.com>
2024-02-18 13:29:00 +0200

    Simple navigation to keep pages after nuxt generate

--- tutorial_site/pages/index.vue
+++ tutorial_site/pages/index.vue
@@ -1,8 +1,22 @@
-<script lang="ts">
+<script setup lang="ts">
+const { data: navigation } = await useAsyncData("navigation", () =>
+  fetchContentNavigation()
+);
 </script>
 
 <template>
-  <ContentDoc path="/index" />
+  <v-card class="mx-auto" max-width="300">
+    <v-list density="compact" nav>
+      <v-list-item
+        v-for="navigationItem in navigation"
+        :key="navigationItem._path"
+        v-if="navigation && Array.isArray(navigation)"
+        :title="navigationItem.title"
+        :subtitle="navigationItem._path"
+        :to="navigationItem._path"
+        nuxt
+      >
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
-
-
