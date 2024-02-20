2b86f71b97ab529bc8ed91c8d2571d37fd5bf82d
ViliusP <viliuspaliokas@gmail.com>
2024-02-20 10:58:44 +0200

    Remove commented code

--- tutorial_site/layouts/default.vue
+++ tutorial_site/layouts/default.vue
@@ -10,38 +10,10 @@
         />
       </v-avatar>
 
-      <!-- <v-app-bar-nav-icon ></v-app-bar-nav-icon> -->
       <v-toolbar-title>{{ t("hello.world") }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
     </v-app-bar>
-
-    <!-- <v-navigation-drawer >
-
-
-      <v-list
-        :lines="false"
-        density="compact"
-        nav
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :value="item"
-          rounded="xl"
-          color="primary"
-        >
-          <template v-slot:prepend>
-            <v-icon :icon="item.icon"></v-icon>
-          </template>
-
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item>
-      </v-list>
-
-
-
-      </v-navigation-drawer> -->
 
     <v-main>
       <v-container fluid class="py-8 px-6">
