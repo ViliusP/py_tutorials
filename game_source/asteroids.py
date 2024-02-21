90c4eddf18a222af2120ed8d6e93a6e07a0cdc08
ViliusP <viliuspaliokas@gmail.com>
2024-02-21 11:58:48 +0200

    Add speed to circle

--- game_source/asteroids.py
+++ game_source/asteroids.py
@@ -1,13 +1,17 @@
 # Example file showing a circle moving on screen
 import pygame
 
+SCREEN_COLOR = (140, 107, 136)
+SPEED_MULTIPLIERS = [1, 1.1, 1.2, 1.5, 1.7, 2]
+BASE_SPEED = 300
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-
+speed_multiplier_index=0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
@@ -16,22 +20,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP] and speed_multiplier_index < len(SPEED_MULTIPLIERS)-1:
+                speed_multiplier_index+=1
+                print(SPEED_MULTIPLIERS[speed_multiplier_index])
+            if keys[pygame.K_DOWN] and speed_multiplier_index > 0:
+                speed_multiplier_index-=1
+                print(SPEED_MULTIPLIERS[speed_multiplier_index])
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(SCREEN_COLOR)
 
     pygame.draw.circle(screen, "red", player_pos, 40)
 
+    VELOCITY = BASE_SPEED * SPEED_MULTIPLIERS[speed_multiplier_index]
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= VELOCITY * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += VELOCITY * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= VELOCITY * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
+        player_pos.x += VELOCITY * dt
+ 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
