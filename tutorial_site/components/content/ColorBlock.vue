8f962570bd7daab162c4c75f0232b0a7d4e42ea8
ViliusP <viliuspaliokas@gmail.com>
2024-02-28 13:20:04 +0200

    lerp section checkpoint

---
+++ tutorial_site/components/content/ColorBlock.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="table-color-block">
+        <!-- Your content here -->
+    </div>
+</template>
+  
+<script setup lang="ts">
+
+
+defineProps({
+    color: {
+        type: String,
+        default: '#ffffff', // Use a default color if none is provided
+    },
+});
+</script>
+  
+<style>
+.table-color-block {
+    width: 100%;
+    height: 100%;
+    background-color: v-bind(color);
+    /* Dynamically set the background color */
+    box-sizing: border-box;
+    /* Ensure padding and border are included in the total width and height */
+    margin: 2px;
+}
+</style>
+  
