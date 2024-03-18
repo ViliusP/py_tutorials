911cb2534ec048f6baa342278a26eaad258a6fa2
ViliusP <viliuspaliokas@gmail.com>
2024-03-18 17:24:52 +0200

    Add MaterialH1-H3 components

---
+++ tutorial_site/components/text/MaterialH2.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="material-h2">
+      <slot />
+    </h1>
+  </template>
+  
+  <style>
+.material-h2 {
+	font-size: 2.8rem !important;
+	font-weight: 400;
+	line-height: 3.25rem;
+	letter-spacing: 0 !important;
+	font-family: "Noto Sans", "Roboto", sans-serif;
+	text-transform: none !important;
+}
+  </style>
+  
---
+++ tutorial_site/components/text/MaterialH3.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="material-h3">
+    <slot />
+  </h1>
+</template>
+
+<style>
+.material-h3 {
+	font-size: 1.5rem !important;
+	font-weight: 600;
+	line-height: 1.625rem;
+	letter-spacing: 0,0073529412em !important;
+	font-family: "Noto Sans", "Roboto", sans-serif;
+	text-transform: none !important;
+}
+</style>
