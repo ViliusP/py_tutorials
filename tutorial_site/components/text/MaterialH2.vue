6b21ee0a4998f9873886e829488e6301ff38711d
ViliusP <viliuspaliokas@gmail.com>
2024-03-18 20:56:55 +0200

    format

--- tutorial_site/components/text/MaterialH2.vue
+++ tutorial_site/components/text/MaterialH2.vue
@@ -1,17 +1,16 @@
 <template>
-    <h2 class="material-h2" v-bind="$attrs">
-      <slot />
-    </h2>
-  </template>
-  
-  <style>
+  <h2 class="material-h2" v-bind="$attrs">
+    <slot />
+  </h2>
+</template>
+
+<style>
 .material-h2 {
-	font-size: 2.8rem !important;
-	font-weight: 400;
-	line-height: 3.25rem;
-	letter-spacing: 0 !important;
-	font-family: "Noto Sans", "Roboto", sans-serif;
-	text-transform: none !important;
+  font-size: 2.8rem !important;
+  font-weight: 400;
+  line-height: 3.25rem;
+  letter-spacing: 0 !important;
+  font-family: "Noto Sans", "Roboto", sans-serif;
+  text-transform: none !important;
 }
-  </style>
-  
+</style>
