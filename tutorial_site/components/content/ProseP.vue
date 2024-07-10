024e3a6ded6ada93f1ff5eeca2edabc2ab86dc78
ViliusP <viliuspaliokas@gmail.com>
2024-07-10 13:58:46 +0300

    Make MaterialP component

--- tutorial_site/components/content/ProseP.vue
+++ tutorial_site/components/content/ProseP.vue
@@ -1,17 +1,15 @@
+<script setup lang="ts">
+import MaterialP from "~/components/text/MaterialP.vue";
+</script>
+
 <template>
-    <p class="prose-p text-justify">
+    <MaterialP class="prose-p text-justify">
         <slot />
-    </p>
+    </MaterialP>
 </template>
 
 <style>
 .prose-p {
-    font-size: 1.375rem !important;
-    font-weight: 300;
-    line-height: 2rem;
-    letter-spacing: 0.002em !important;
-    font-family: "Noto Sans", "Roboto", sans-serif;
-    text-transform: none !important;
     margin-top: 1.375rem;
     margin-bottom: 1.375rem;
 }
