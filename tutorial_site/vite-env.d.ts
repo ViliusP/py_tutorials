2354f1dde0987c38c57b9aca6949032b39c46222
ViliusP <viliuspaliokas@gmail.com>
2024-09-18 14:23:09 +0300

    Add cloudinary sdk

--- tutorial_site/vite-env.d.ts
+++ tutorial_site/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+    readonly GENERATE_REMOTE_BLURHASH: boolean
+    // more env variables...
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
